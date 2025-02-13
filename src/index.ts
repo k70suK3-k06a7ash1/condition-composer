e23a07910f23d36ecab696e485c884dfab00b587
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-02-13 21:15:38 +0900

    chore

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
+export type ComposedFunction<T, V> = (arg: T) => V;
+
+export type ComposedPredicate<T> = ComposedFunction<T, boolean>;
+
 export const composeAll =
-	<T>(...funcs: Array<(_arg: T) => boolean>) =>
-	(_arg: T): boolean =>
-		funcs.every((func) => func(_arg));
+	<T>(...funcs: Array<ComposedPredicate<T>>): ComposedPredicate<T> =>
+	(arg: T): boolean =>
+		funcs.every((func) => func(arg));
 
 export const composeAllNot =
-	<T>(...funcs: Array<(_arg: T) => boolean>) =>
-	(_arg: T): boolean =>
-		funcs.every((func) => !func(_arg));
+	<T>(...funcs: Array<ComposedPredicate<T>>): ComposedPredicate<T> =>
+	(arg: T): boolean =>
+		funcs.every((func) => !func(arg));
 
 export const composeSome =
-	<T>(...funcs: Array<(_arg: T) => boolean>) =>
-	(_arg: T): boolean =>
-		funcs.some((func) => func(_arg));
+	<T>(...funcs: Array<ComposedPredicate<T>>): ComposedPredicate<T> =>
+	(arg: T): boolean =>
+		funcs.some((func) => func(arg));
